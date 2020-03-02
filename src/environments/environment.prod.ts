8fe38c6316f5714b77135c357306faf0f484dfde
harrischelsea <harix_vazdar@hotmail.com>
2020-03-02 21:40:08 +0100

    environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyAMvd7U_hhyUOQJ-90ZMrj2KAnSwkQjDcs",
+    authDomain: "test-ea2c1.firebaseapp.com",
+    databaseURL: "https://test-ea2c1.firebaseio.com",
+    projectId: "test-ea2c1",
+    storageBucket: "test-ea2c1.appspot.com",
+    messagingSenderId: "1038478609619",
+    appId: "1:1038478609619:web:b4e80173b301679dd85826"
+  }
 };
