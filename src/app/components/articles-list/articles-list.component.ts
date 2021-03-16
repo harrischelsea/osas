a5aef6ef921c2564f3ac67e774bbb6a7d5180964
harrischelsea <harix_bazdar@hotmail.com>
2021-03-16 19:59:00 +0100

    sort articles

--- src/app/components/articles-list/articles-list.component.ts
+++ src/app/components/articles-list/articles-list.component.ts
@@ -17,12 +17,23 @@ export class ArticlesListComponent implements OnInit {
     this.store.pipe(select(articleSelectors.articlesSelector)).subscribe(values => {
       const { articles } = values;
       if(!articles) return;
-      this.ARTICLES = articles;
+      // this.ARTICLES = articles;
+      this.ARTICLES = articles.slice().sort(this.sortiranje);
       this.POPULAR = articles.slice().sort(this.compare).slice(0, 5);
     })
    }
 
   ngOnInit() {
+  }
+
+  sortiranje(a, b) {
+    let comparison = 0;
+    if (moment(new Date(a.datum)) > moment(new Date(b.datum))) {
+      comparison = -1;
+    } else if (moment(new Date(a.datum)) > moment(new Date(b.datum))) {
+      comparison = 1;
+    }
+    return comparison;
   }
 
   compare(a, b) {
